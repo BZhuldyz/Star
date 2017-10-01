bc3e42c80682cef333fc6e226568f78ab33b9830
Zhuldyz Bekbolat <Zhbekbolat@gmail.com>
2017-10-02 01:52:37 +0600

    Екинши коммит

--- src/Zhuke/Hello.java
+++ src/Zhuke/Hello.java
@@ -3,5 +3,7 @@ package Zhuke;
 public class Hello {
 public static void main(String [] args){
     System.out.print("Hello World!");
+    System.out.print("Бул зертханалык жумыстын максаты");
+    System.out.print("GitGup- пен жумыс жасап уйрену");
 }
 }
