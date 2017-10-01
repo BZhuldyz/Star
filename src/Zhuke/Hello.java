18d9d11b3484b45f9cecaa7b5513e4136a5fc459
Zhuldyz Bekbolat <Zhbekbolat@gmail.com>
2017-10-02 02:05:46 +0600

    Hello.java аздап озгерди

--- src/Zhuke/Hello.java
+++ src/Zhuke/Hello.java
@@ -5,5 +5,10 @@ public static void main(String [] args){
     System.out.print("Hello World!");
     System.out.print("Бул зертханалык жумыстын максаты");
     System.out.print("GitGup- пен жумыс жасап уйрену");
+    int a= MyMath.kvadrat( 5);
+    int b=MyMath.kup( 5) ;
+    System.out.print("a = " + a);
+    System.out.print("b = " + b);
+
 }
 }
