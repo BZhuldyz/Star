1af0093b5e022953fd7d37d99a921d10db70ddf1
Zhuldyz Bekbolat <Zhbekbolat@gmail.com>
2017-10-02 02:13:35 +0600

    Hello.java аздап озгерди

--- src/Zhuke/Hello.java
+++ src/Zhuke/Hello.java
@@ -9,6 +9,6 @@ public static void main(String [] args){
     int b=MyMath.kup( 5) ;
     System.out.print("a = " + a);
     System.out.print("b = " + b);
-
+    System.out.print(MyMath.max(8,9));
 }
 }
